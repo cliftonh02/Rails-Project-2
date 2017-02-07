e26b2d1d0ebcf1a127e99cb8bbc76b23a97668f4
Frontlines 3 <cliftonh02@gmail.com>
2017-02-07 14:57:34 -0500

    adding crud

--- Checklist/app/controllers/items_controller.rb
+++ Checklist/app/controllers/items_controller.rb
@@ -1,2 +1,39 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Items.all.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(params.require(:item).permit(:title, :description))
+    if item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :description)
+end
+
+def find_item
+  @item = Item.find(params[:id])
+end
 end
